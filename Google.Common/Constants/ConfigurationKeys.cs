5beaaf44442dfd0abda558e6c7d469d0f50feba6
VietNT <tienviet.nguyen@digimed.vn>
2018-08-06 17:00:58 +0700

    update

--- Google.Common/Constants/ConfigurationKeys.cs
+++ Google.Common/Constants/ConfigurationKeys.cs
@@ -7,5 +7,6 @@
         public const string DefaultConnection = "DefaultConnection";
         public const string UploadFolder = "/uploads/";
         public const string EmptyGuild = "EmptyGuild";
+        public const string DefaultAssetId = "EmptyGuild";
     }
 }
