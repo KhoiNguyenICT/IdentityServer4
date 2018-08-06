0d9e98ce4555b811435d329caab20e9217757e27
khoinguyenict@gmail.com <khoinguyenict@gmail.com>
2018-08-06 15:09:13 +0700

    update resize image

---
+++ Google.Application/Extensions/ImageResizerMiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Google.Application.Configurations.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Google.Application.Extensions
+{
+    public static class ImageResizerMiddlewareExtensions
+    {
+        public static IServiceCollection AddImageResizer(this IServiceCollection services)
+        {
+            return services.AddMemoryCache();
+        }
+
+        public static IApplicationBuilder UseImageResizer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ImageResizerMiddleware>();
+        }
+    }
+}
